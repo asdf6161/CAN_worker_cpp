3d3fc08cd229f5b84f10811d8f67fea4d343d7f6
yura <sk12.yura@gmail.com>
2019-01-27 09:51:37 +0700

    HAL gen init

---
+++ cpp/CANWorker.cpp
@@ -0,0 +1,21 @@
+/*
+ * CANWorker.cpp
+ *
+ *  Created on: 27 џэт. 2019 у.
+ *      Author: yura
+ */
+
+#include <CANWorker.h>
+
+namespace canw {
+
+CAN_Worker::CAN_Worker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CAN_Worker::~CAN_Worker() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace canw */
---
+++ cpp/CANWorker.h
@@ -0,0 +1,21 @@
+/*
+ * CANWorker.h
+ *
+ *  Created on: 27 џэт. 2019 у.
+ *      Author: yura
+ */
+
+#ifndef CANWORKER_H_
+#define CANWORKER_H_
+
+namespace canw {
+
+class CAN_Worker {
+public:
+	CAN_Worker();
+	virtual ~CAN_Worker();
+};
+
+} /* namespace canw */
+
+#endif /* CANWORKER_H_ */
